f33abb443876b130d441a804f87691d9e78925e4
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-29 21:57:35 -0400

    add daylistitem story

--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-
+import DayListItem from '../src/components/DayListItem';
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
 
-import Button from "components/Button";
 
+import Button from "components/Button";
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -21,4 +20,16 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+
+  storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
