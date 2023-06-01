fe5e4125319f06592afb3dfa058f9e65bebcd808
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-31 20:10:42 -0400

    Updating the code onChange & Value

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,47 +4,52 @@ import "components/Application.scss";
 import DayList from "./DayList";
 
 const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
+{
+id: 1,
+name: "Monday",
+spots: 2,
+},
+{
+id: 2,
+name: "Tuesday",
+spots: 5,
+},
+{
+id: 3,
+name: "Wednesday",
+spots: 0,
+},
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+const [value, setValue] = useState("Monday");
 
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
+return (
+<main className="layout">
+<section className="sidebar">
+<img
+       className="sidebar--centered"
+       src="images/logo.png"
+       alt="Interview Scheduler"
+     />
+<hr className="sidebar__separator sidebar--centered" />
+<nav className="sidebar__menu">
+<DayList days={days} value={value} onChange={setValue} />
+</nav>
+<img
+       className="sidebar__lhl sidebar--centered"
+       src="images/lhl.png"
+       alt="Lighthouse Labs"
+     />
+</section>
+<section className="schedule">
+{/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+</section>
+</main>
+);
 }
+
+
+
+
+
