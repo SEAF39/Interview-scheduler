94a2fcf1261bffaa36d1f66efeb32b53ff39cfdb
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-02 02:30:18 -0400

    Build appointment components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+/* index.js */
+import React from "react";
+import "./styles.scss";
+
+
+const Appointment = (props) => {
+  return <article className="appointment"></article>;
+};
+
+export default Appointment;
