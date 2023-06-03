c7c2b1c9ef9c1068b617342d0820527c5fcdec9c
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-03 01:12:28 -0400

    fix the function with props

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,40 @@
+/* Form.js */
+import React from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+const Form = (props) => {
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={props.name}
+            onChange={props.onChange}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={props.interviewer}
+          onChange={props.onInterviewerChange}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={props.onCancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={props.onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+};
+
+export default Form;
