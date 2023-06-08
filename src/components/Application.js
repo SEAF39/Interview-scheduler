27d5df12d0840587d41b4d26a5d616c6c29f0e35
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-08 01:32:08 -0400

    add Appointment Data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,56 +1,107 @@
 /* Application.js */
-import React, { useState } from "react";
-import "components/Application.scss";
+
+import React, { Component } from "react";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
+import "components/Application.scss";
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
-const days = [
-{
-id: 1,
-name: "Monday",
-spots: 2,
-},
-{
-id: 2,
-name: "Tuesday",
-spots: 5,
-},
-{
-id: 3,
-name: "Wednesday",
-spots: 0,
-},
-];
+class Application extends Component {
+  state = {
+    day: "Monday",
+    days: ["Monday", "Tuesday", "Wednesday"],
+    appointments,
+    interviewers: {}
+  };
 
-export default function Application(props) {
-const [value, setValue] = useState("Monday");
+  renderAppointments() {
+    const { day, appointments } = this.state;
+    const dailyAppointments = appointments[day];
 
-return (
-<main className="layout">
-<section className="sidebar">
-<img
-       className="sidebar--centered"
-       src="images/logo.png"
-       alt="Interview Scheduler"
-     />
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList days={days} value={value} onChange={setValue} />
-</nav>
-<img
-       className="sidebar__lhl sidebar--centered"
-       src="images/lhl.png"
-       alt="Lighthouse Labs"
-     />
-</section>
-<section className="schedule">
-{/* Replace this with the schedule elements during the "The Scheduler" activity. */}
-</section>
-</main>
-);
+    if (!dailyAppointments) {
+      return null;
+    }
+
+    return dailyAppointments.map(appointment => (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={appointment.interview}
+      />
+    ));
+  }
+
+  setDay = day => {
+    this.setState({ day });
+  };
+
+  render() {
+    const { days } = this.state;
+
+    return (
+      <main className="layout">
+        <section className="sidebar">
+          <img
+            className="sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
+          />
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList days={days} value={this.state.day} onChange={this.setDay} />
+          </nav>
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
+          />
+        </section>
+        <section className="schedule">
+          {this.renderAppointments()}
+          <Appointment key="last" time="5pm" />
+        </section>
+      </main>
+    );
+  }
 }
 
-
-
-
-
+export default Application;
