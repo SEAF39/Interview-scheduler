63d97a6b85b28eafc7008039a93564759ed32d31
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-11 20:53:39 -0400

    Combined State

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,54 +1,25 @@
 /* Application.js */
-import React, { useEffect, useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-export default function Application() {
-  const [days, setDays] = useState([]);
 
-  const appointments = {
-    "1": {
-      id: 1,
-      time: "12pm",
-    },
-    "2": {
-      id: 2,
-      time: "1pm",
-      interview: {
-        student: "Lydia Miller-Jones",
-        interviewer: {
-          id: 3,
-          name: "Sylvia Palmer",
-          avatar: "https://i.imgur.com/LpaY82x.png",
-        },
-      },
-    },
-    "3": {
-      id: 3,
-      time: "2pm",
-    },
-    "4": {
-      id: 4,
-      time: "3pm",
-      interview: {
-        student: "Archie Andrews",
-        interviewer: {
-          id: 4,
-          name: "Cohana Roy",
-          avatar: "https://i.imgur.com/FK8V841.jpg",
-        },
-      },
-    },
-    "5": {
-      id: 5,
-      time: "4pm",
-    },
+export default function Application() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = (day) => {
+    setState((prev) => ({ ...prev, day }));
   };
 
-  const [value, setValue] = useState("Monday");
+  const setDays = (days) => {
+    setState((prev) => ({ ...prev, days }));
+  };
 
   useEffect(() => {
     axios
@@ -71,7 +42,11 @@ export default function Application() {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={value} onChange={setValue} />
+          <DayList
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -80,7 +55,7 @@ export default function Application() {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => (
+        {Object.values(state.appointments).map((appointment) => (
           <Appointment key={appointment.id} {...appointment} />
         ))}
         <Appointment key="last" time="5pm" />
@@ -88,3 +63,8 @@ export default function Application() {
     </main>
   );
 }
+
+
+
+
+
