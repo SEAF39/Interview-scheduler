d307eb7ed5ed4a97ee23137cb59dcefcc01701fa
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-13 05:02:41 -0400

    Creating Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,10 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 import "components/Application.scss";
+
+
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import {
@@ -11,48 +14,64 @@ import {
   getInterview,
 } from "helpers/selectors";
 
-
 export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
 
   const setDay = (day) => {
-    setState((prev) => ({ ...prev, day }));
+    setState((prevState) => ({ ...prevState, day }));
   };
 
   useEffect(() => {
-    const fetchAppointments = axios.get('http://localhost:8001/api/appointments');
-    const fetchDays = axios.get('http://localhost:8001/api/days');
-    const fetchInterviewers = axios.get('http://localhost:8001/api/interviewers');
-  
-    Promise.all([fetchDays, fetchAppointments, fetchInterviewers])
-      .then((responses) => {
-        const [daysResponse, appointmentsResponse, interviewersResponse] = responses;
-        console.log(daysResponse.data); // response from days API call
-        console.log(appointmentsResponse.data); // response from appointments API call
-        console.log(interviewersResponse.data); // response from interviewers API call
-        
-        // Update the state with the retrieved data
-        setState(prevState => ({
+    const fetchData = async () => {
+      try {
+        const [daysResponse, appointmentsResponse, interviewersResponse] =
+          await Promise.all([
+            axios.get('http://localhost:8001/api/days'),
+            axios.get('http://localhost:8001/api/appointments'),
+            axios.get('http://localhost:8001/api/interviewers'),
+          ]);
+
+        setState((prevState) => ({
           ...prevState,
           days: daysResponse.data,
           appointments: appointmentsResponse.data,
-          interviewers: interviewersResponse.data
+          interviewers: interviewersResponse.data,
         }));
-      })
-      .catch((error) => {
+      } catch (error) {
         console.log('Error:', error);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const bookInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    setState((prevState) => ({
+      ...prevState,
+      appointments,
+    }));
+  };
+
   const interviewersList = getInterviewersForDay(state, state.day);
 
   const schedule = getAppointmentsForDay(state, state.day).map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-  
+
     return (
       <Appointment
         key={appointment.id}
@@ -60,11 +79,10 @@ export default function Application() {
         time={appointment.time}
         interview={interview}
         interviewers={interviewersList}
-
+        bookInterview={bookInterview}
       />
     );
   });
-  
 
   return (
     <main className="layout">
@@ -91,6 +109,7 @@ export default function Application() {
     </main>
   );
 }
+
 
 
 
