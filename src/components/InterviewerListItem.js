c3ae7da39cb4649e1aa59a61dbbca724fe68bb0f
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-18 01:29:10 -0400

    refactoring the code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,6 +1,35 @@
 /* InterviewerListItem.js */
 
 import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
+
+
+
+
+
+
+
+
+
+/* import React from "react";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
@@ -20,3 +49,4 @@ export default function InterviewerListItem(props) {
     </li>
   );
 }
+ */
