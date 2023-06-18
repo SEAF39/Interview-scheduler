04a878cda4a0f06221ba588e2ff90ab5f1e780e5
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-18 01:25:32 -0400

    refactor the code fix the code issue transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -23,32 +23,3 @@ export default function useVisualMode(initial) {
   return { mode, transition, back };
 }
 
-
-
-/* 
-import { useState } from "react";
-
-export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
-  
-  const transition = (newMode, replace = false) => {
-    setMode(newMode);
-
-    if (replace) {
-      setHistory(() => [...history.slice(0, history.length - 1), newMode]);
-    } else {
-      setHistory(prev => [...prev, newMode]);
-    }
-  };
-  const back = () => {
-    if (history.length > 1) {
-      const newHistory = history.slice(0, -1);
-      setMode(newHistory[newHistory.length - 1]);
-      setHistory(() => [...newHistory]);
-    }
-  };
-  return { mode, transition, back };
-}
-
- */
