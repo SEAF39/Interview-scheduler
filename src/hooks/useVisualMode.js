0f559b7ac0f3c078b1eafeb9afdc3e9d6a16aa41
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-18 01:24:55 -0400

    refactor the code fix the code issue transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,26 +6,49 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode, replace = false) {
-    if (replace) {
-      setMode(newMode);
-      setHistory(prevHistory => {
-        const newHistory = [...prevHistory];
-        newHistory[newHistory.length - 1] = newMode;
-        return newHistory;
-      });
-    } else {
-      setMode(newMode);
-      setHistory(prevHistory => [...prevHistory, newMode]);
-    }
-  }
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    setHistory(prevHistory =>
+      replace ? [...prevHistory.slice(0, -1), newMode] : [...prevHistory, newMode]
+    );
+  };
 
-  function back() {
+  const back = () => {
     if (history.length > 1) {
       setMode(history[history.length - 2]);
-      setHistory(prevHistory => prevHistory.slice(0, prevHistory.length - 1));
+      setHistory(prevHistory => prevHistory.slice(0, -1));
     }
-  }
+  };
 
   return { mode, transition, back };
 }
+
+
+
+/* 
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+
+    if (replace) {
+      setHistory(() => [...history.slice(0, history.length - 1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
+  };
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, -1);
+      setMode(newHistory[newHistory.length - 1]);
+      setHistory(() => [...newHistory]);
+    }
+  };
+  return { mode, transition, back };
+}
+
+ */
