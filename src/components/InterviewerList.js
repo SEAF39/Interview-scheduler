d3c7e0c8af7035de071875c11dcdf5f929a2a044
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-18 01:30:34 -0400

    refactor the code fix interveiwList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,19 @@
 /* InterviewerList.js */
+
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  const { value, onChange, interviewers } = props;
+  const { interviewers, interviewer, onChange } = props;
 
-  const interviewerItems = interviewers.map((interviewer) => (
-    <InterviewerListItem 
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === value}
-      setInterviewer={() => onChange(interviewer.id)}    
+  const interviewerItems = interviewers.map((interviewerObj) => (
+    <InterviewerListItem
+      key={interviewerObj.id}
+      name={interviewerObj.name}
+      avatar={interviewerObj.avatar}
+      selected={interviewerObj.id === interviewer}
+      setInterviewer={() => onChange(interviewerObj.id)}
     />
   ));
 
@@ -23,3 +24,49 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+    <InterviewerListItem 
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={any => props.onChange(interviewer.id)}    
+    />
+  );
+    });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
+ */
