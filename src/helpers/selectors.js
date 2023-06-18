13c8931df93ffcc74ce03eda8cad77b39be3b202
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-18 01:28:17 -0400

    refactoring the code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -5,32 +5,24 @@ export function getAppointmentsForDay(state, day) {
   if (!selectedDay) {
     return [];
   }
-
   const appointmentIds = selectedDay.appointments;
-
   const appointmentsForDay = appointmentIds.map((appointmentId) => {
     return state.appointments[appointmentId];
   });
-
   return appointmentsForDay;
 }
-
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
   }
-
   const { student, interviewer } = interview;
-
   return {
     student,
     interviewer: state.interviewers[interviewer]
   };
 }
-
 export function getInterviewersForDay(state, day) {
   const selectedDay = state.days.find((d) => d.name === day);
-
   if (!selectedDay) {
     return [];
   }
