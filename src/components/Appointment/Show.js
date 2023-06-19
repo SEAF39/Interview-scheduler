168e191a95f4a9f4529a93f104519190e825e14e
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-19 00:38:27 -0400

    refactor show component

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -6,6 +6,7 @@ export default function Show({ student, interviewer, id, onEdit, onDelete }) {
   const handleEdit = () => {
     onEdit({ student, interviewer, id });
   };
+  console.log("Interviewer:", interviewer);
 
   const handleDelete = () => {
     onDelete(id);
@@ -17,7 +18,7 @@ export default function Show({ student, interviewer, id, onEdit, onDelete }) {
         <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewer.name}</h3>
+          <h3 className="text--regular">{interviewer && interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
@@ -39,3 +40,5 @@ export default function Show({ student, interviewer, id, onEdit, onDelete }) {
     </main>
   );
 }
+
+
