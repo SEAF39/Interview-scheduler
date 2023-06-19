203a8802c59f6d0a8c6fa97f270d1418c7b20ff7
SEAF39 <labber@DESKTOP-7J1C764>
2023-06-19 01:24:34 -0400

    add & import props types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,47 @@
 /* InterviewerList.js */
 
 import React from "react";
+import PropTypes from "prop-types";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const { interviewers, interviewer, onChange } = props;
+
+  const interviewerItems = interviewers.map((interviewerObj) => (
+    <InterviewerListItem
+      key={interviewerObj.id}
+      name={interviewerObj.name}
+      avatar={interviewerObj.avatar}
+      selected={interviewerObj.id === interviewer}
+      setInterviewer={() => onChange(interviewerObj.id)}
+    />
+  ));
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerItems}</ul>
+    </section>
+  );
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  interviewer: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+};
+
+
+
+/* 
+import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
@@ -26,7 +67,7 @@ export default function InterviewerList(props) {
 }
 
 
-
+ */
 
 
 
